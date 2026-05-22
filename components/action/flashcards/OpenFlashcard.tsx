cb186ef2e4a699b5bca0ca36720881dbce20ec85
Ayush Kumar <167435819+AYUSH-pro-grammer@users.noreply.github.com>
2026-05-23 02:39:13 +0530

    msg

--- components/action/flashcards/OpenFlashcard.tsx
+++ components/action/flashcards/OpenFlashcard.tsx
@@ -1,15 +1,55 @@
 'use client'
+import { useEffect, useState } from "react"
+import styles from "./openFlashcard.module.css"
+
+
 
 type flashcard = {
     title: string
-    
+    document_id: number 
+    desp: string 
+    id:number 
+    user_id: number
+}
+
+type propsData = {
+    data: flashcard[]
+    onClose: () => void
 }
 
 
-export default function OpenFlashCard(){
+export default function OpenFlashCard({data, onClose}: propsData){
+
+    const [open, setOpen] = useState(false)
+
+    useEffect(() => {
+        setOpen(true)
+    }, [])
+
+
+
+    const handleOff = () => {
+        setOpen(false)
+
+        setTimeout(()=>{
+            onClose()
+        }, 300)
+    }
+
+    
+
+
     return (
-        <div>
-            Opened
+        <div className={`${styles.box} ${open ? styles.open : styles.close}`}>
+            Opened 
+
+            {data.map((i)=>{
+                return(
+                    <div>{i.title}</div>
+                )
+            })}
+
+            <button onClick={handleOff} >Close</button>
         </div>
     )
 }
